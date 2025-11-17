a87b84ee60ad3891f5bc710af482b1171cf1a05b
NinaHZhang <ninazhang.professional@gmail.com>
2025-11-17 10:16:03 -0500

    wrote barebones ppo rollout, initialized stuff

---
+++ ppo.py
@@ -0,0 +1,123 @@
+import torch
+import numpy as np
+from torch.optim import Adam
+from network import FeedForwardNN
+from torch.distributions import MultivariateNormal
+
+class PPO:
+    '''PPO class used to train agent'''
+
+    def __init__(self, env):
+        '''Initializes PPO model including hyperparameters.
+        parameters: env - the environment to train on'''
+       
+        #extract environment information
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.act_dim = env.action_space.shape[0]
+
+        self.init_hyperparameters()
+
+        #initialize actor and critic networks
+        self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
+        self.critic = FeedForwardNN(self.obs_dim, 1)
+
+        #create a variable for matrix, 0.5 for stdev is arbitrary, used to generate the covariance matrix
+        self.cov_var = torch.full(size=(self.act_dim,), fill_value = 0.5)
+
+        #create the covariance matrix, which will be used for multivariate norm distr
+        self.cov_mat = torch.diag(self.cov_var)
+
+    def get_action(self, obs):
+        '''samples an action to create a multivariate normal distribution, using the mean found through the actor network inputting an observation. 
+        samples an action from the distribution and its probability, returns it as a numpy array'''
+        #querying the neural network for a mean action
+        mean = self.actor(obs)
+        
+        #create a multivariate normal distribution
+        dist = MultivariateNormal(mean, self.cov_mat)
+
+        #sample an action from the distribution and get its log probability
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+
+        #convert action from tensor to numpy array
+        return action.detach().numpy(), log_prob.detach()
+    def _init_hyperparameters(self):
+
+        #hyperparameters
+        self.lir = 3e-4   #learning rate
+        self.gamma = 0.99  #discount factor
+        self.lam = 0.95   #lambda for gae
+        self.clip = 0.2  #clip parameter for ppo
+        self.epochs = 10  #number of epochs per update
+        self.batch_size = 64   #mini-batch size
+        self.timesteps_per_batch = 2048    #timesteps per batch
+
+    def rollout(self):
+        batch_obs = []  #batch state/observations
+        batch_acts = [] #batch actions
+        batch_log_probs = [] #log probability of each action
+        batch_rews = [] # batch rewards (r_t)
+        batch_rtgs = [] #batch rewards-to-go (aka, return R_t with discounts)
+        batch_lens = [] #episode length, steps in eps
+        
+        #number of timesteps run in this batch
+        while t < self.timesteps_per_batch:
+            ep_rews = [] #rewards this episode
+
+            obs = self.env.reset()
+            done = False
+
+            for ep_t in range(self.max_timesteps_per_episde):
+                t+=1
+
+                batch_obs.append(obs) #collect observation
+                action, log_prob = self.get_action(obs) #get action and action probabilities from actor
+                obs, rew, done, _ = self.env.step(action)
+
+                #collect reward, action and log prob
+                ep_rews.append(rew)
+                batch_acts.append(action)
+                batch_log_probs.append(log_prob)
+                
+                if done:
+                    break
+        
+        #collect episodic length and the rewards
+        batch_lens.append(ep_t + 1) #because timesteps start at 0
+        batch_rews.append(ep_rews)
+
+        #reshape data as tensors in shape specified
+        batch_obs = torch.tensor(batch_obs, dtype=torch.float)
+        batch_acts = torch.tensor(batch_acts, dtype=torch.float)
+        batch_log_probs = torch.tensor(batch_log_probs, dtype=torch.float)
+
+        #compute rewardstogo/return
+        batch_rtgs = self.compute_rtgs(batch_rews)
+
+        #return the batch data
+        return batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens
+
+    def compute_rtgs(self, batch_rews):
+        #have to return an array of rtg per episode per batch to return
+        batch_rtgs = []
+
+        #iterate through each episode in the batch
+        for ep_rews in reversed(batch_rews):
+            discounted_reward = 0
+
+            for rew in reversed(ep_rews): #loop through each timestep in the episode
+                discounted_reward = rew + discounted_reward * self.gamma
+                batch_rtgs.insert(0, discounted_reward)
+
+        #convert the rtg into a tensor
+        batch_rtgs = torch.tensor(batch_rtgs, dtype=torch.float)
+        return batch_rtgs
+
+    def learn(self, total_timesteps):
+        t_so_far = 0 #timesteps simulated so far
+        while t_so_far < total_timesteps:
+            #incrememnt t_so_far somewhere
+            batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = self.rollout()
+
